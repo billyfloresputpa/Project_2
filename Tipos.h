8d305f15d2dacbbebd0f90358fcab2e3b01a19d7
billyfloresputpa <51890804+billyfloresputpa@users.noreply.github.com>
2019-07-02 10:29:21 -0500

    Create Tipos.h

---
+++ Tipos.h
@@ -0,0 +1,16 @@
+//
+// Created by ruben on 6/10/19.
+//
+
+#ifndef AGREGACION_TIPOS_H
+#define AGREGACION_TIPOS_H
+#include<limits>
+
+#include <string>
+using namespace std;
+// Definiendo alias
+using TipoEntero = int;
+using TipoCaracter = char;
+using TipoString = string;
+
+#endif //AGREGACION_TIPOS_H
