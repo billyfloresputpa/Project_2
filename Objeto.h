87d0b24136dc36777f3f92616212af7dff8e81a0
billyfloresputpa <51890804+billyfloresputpa@users.noreply.github.com>
2019-07-02 10:30:21 -0500

    Create Objeto.h

---
+++ Objeto.h
@@ -0,0 +1,35 @@
+/ Created by ruben on 6/10/19.
+//
+
+#ifndef AGREGACION_OBJETO_H
+#define AGREGACION_OBJETO_H
+
+
+#include <iostream>
+#include "Tipos.h"
+
+using namespace std;
+
+class Objeto {
+private:
+    string        nombre;
+    TipoCaracter  color;
+    TipoEntero    posX;
+    TipoEntero    posY;
+public:
+    Objeto();
+    Objeto(const TipoString& nombre, TipoCaracter color,
+           TipoEntero posX, TipoEntero posY);
+    virtual ~Objeto();
+    void setNombre(const TipoString& nombre);
+    string     getNombre();
+    TipoEntero getPosX();
+    TipoEntero getPosY();
+    void setPosX(int PosX_){posX=PosX_;};
+    void setPosY(int PosY_){posY=PosY_;};
+    char getColor();
+    void moverse(TipoEntero x, TipoEntero y);
+    string mostrarPosicion();
+};
+
+#endif //AGREGACION_OBJETO_H
