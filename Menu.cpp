be0e2613c9352b5c507f2b9e5a52aa7b4d771ea1
billyfloresputpa <51890804+billyfloresputpa@users.noreply.github.com>
2019-07-02 22:09:02 -0500

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-enum class Opciones { Agregar=1, Remover, Mostrar, Buscar, Mover};
+enum class Opciones { Agregar=1, Remover, Mostrar, Buscar, Mover,Boss};
 
 
 void limpiar() {
@@ -33,7 +33,8 @@ void Menu::imprimirMenu() {
     cout << "2. Remover objeto\n";
     cout << "3. Dibujar Mapa\n";
     cout<< "4. Buscar Robot\n";
-    cout<<" 5. Mover Objeto\n\n";
+    cout<<"5. Mover Objeto\n";
+    cout<<"6. Agregar Boss\n\n";
     cout << "0. Para Salir\n\n";
 }
 
@@ -135,7 +136,7 @@ void Menu::generaobstaculos (int numobstaculos)
                 cont+=item->getPosX()==x && item->getPosY()==y;
             }
         }while(cont!=0);
-        tierra.adicionarObjeto(new Objeto("Obstaculo", '@', x, y));
+        tierra.adicionarObjeto(new Objeto("Obstaculo", '@', x, y,"Obstaculo"));
     }
 }
 
@@ -157,28 +158,102 @@ void Menu::buscarObjeto()
 }
 
 void Menu::moverobjeto() {
-    TipoEntero cont = 0,x,y;
+    TipoEntero cont1 = 0,cont2=0,x,y;
     TipoCaracter color;
     TipoString nombre = input<TipoString>("Ingrese Nombre del Robot a mover");
     for (auto &item: tierra.objetos) {
         if (nombre == item->getNombre()) {
-            cont = 1;
+            do {
+                cont2=0;
+                x = input<TipoEntero>("Ingrese nueva posicion X : ");
+                y = input<TipoEntero>("Ingrese nueva posicion Y : ");
+                for(auto &item2: tierra.objetos) {
+                    if (item2->getPosY() == y && item2->getPosX() == x){
+                        cont2++;
+                    cout << endl << "Ha seleccionado una pocision ocupada" << endl;
+                } }
+
+            }while(cont2!=0);
+            item->setPosX(x);
+            item->setPosY(y);
+
+            cont1 ++;
             break;
         }
 
+
     }
-    if (cont==1)
+    if(cont1==0)
     {
-            x = input<TipoEntero>("Ingrese nueva posicion X : ");
-
-            y = input<TipoEntero>("Ingrese nueva posicion Y : ");
-        for(auto &item : tierra.objetos) {
-            cont += item->getPosX() == x && item->getPosY() == y;
-        }
-
-    tierra.adicionarObjeto(new Objeto(nombre, color, x, y));
+        cout<<"Ese robot no existe";
     }
 }
+
+
+
+void Menu::ingresaBoss()
+{
+    TipoEntero x,y,xn,yn,cont=0,cont2=0;
+    TipoString nombre;
+    TipoCaracter color;
+    x = input<TipoEntero>("Ingrese posición X (Esquina superior Izquierda) de Robot Boss : ");
+    y = input<TipoEntero>("Ingrese posición Y (Esquina superior Izquierda) de Robot Boss : ");
+    xn = input<TipoEntero>("Ingrese el tamaño horizontal del Robot Boss: ");
+    yn = input<TipoEntero>("Ingrese el tamaño vertical del Robot Boss");
+
+    do{
+        cont=0;
+        nombre = input<TipoString>("Ingrese Nombre : ");
+        for(auto &item : tierra.objetos)
+        {
+            cont+=item->getNombre()==nombre;
+        }
+    }while(cont!=0);
+
+    do{
+        cont=0;
+        color  = input<TipoCaracter>("Ingrese color (Un caracter): ");
+        for(auto &item : tierra.objetos)
+        {
+            cont+=item->getColor()== color;
+        }
+    }while(cont!=0);
+
+
+    for(TipoEntero f=y;f<y+yn-1;f++)
+    {
+        for(TipoEntero c=x;c<x+xn;c++)
+        {
+            for(auto& item:tierra.objetos)
+            {
+                if(item->getPosY()==f&&item->getPosX()==c) {
+                    cont2++;
+                    break;
+                }
+            }if(cont2!=0)
+                break;
+        }if(cont2!=0)
+            break;
+    }if (cont2!=0){
+        cout<<endl<<"Ya existe un robot en esa area"<<endl;
+    }
+    if(cont2==0)
+    {
+
+        for(TipoEntero f=y;f<y+yn;f++)
+        {
+            for(TipoEntero c=x;c<x+xn;c++)
+            {
+                if((f==x)&&(c==y)){
+                    tierra.adicionarObjeto(new Objeto(nombre, color, x, y,"Boss"));
+                    continue;}
+                tierra.adicionarObjeto(new Objeto(nombre, color, c, f,"BossTile"));
+
+            }
+        }
+    }
+}
+
 
 
 void Menu::seleccionarOpcion() {
@@ -199,6 +274,9 @@ void Menu::seleccionarOpcion() {
       case Opciones::Mover: //Mover Objeto
             moverobjeto();
             break;
+      case Opciones::Boss:
+            ingresaBoss();
+            break;
+
     }
 }
-
