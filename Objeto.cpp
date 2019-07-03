bec86c3b2c8aca905bea92f6804fbcf2211681d7
billyfloresputpa <51890804+billyfloresputpa@users.noreply.github.com>
2019-07-02 22:12:02 -0500

    Update Objeto.cpp

--- Objeto.cpp
+++ Objeto.cpp
@@ -10,6 +10,10 @@ Objeto::Objeto(const TipoString& nombre, TipoCaracter color,
                TipoEntero posX, TipoEntero posY):
                                                  nombre{nombre}, color{color},
                                                  posX{posX}, posY{posY} {}
+Objeto::Objeto(const TipoString& nombre, TipoCaracter color,
+               TipoEntero posX, TipoEntero posY,TipoString state):
+        nombre{nombre}, color{color},
+        posX{posX}, posY{posY},state{state} {}
 
 Objeto::~Objeto() {}
 
@@ -20,7 +24,14 @@ TipoString   Objeto::getNombre() { return nombre; }
 TipoEntero   Objeto::getPosX()   { return posX; }
 TipoEntero   Objeto::getPosY()   { return posY; }
 TipoCaracter Objeto::getColor()  { return color; }
+TipoEntero Objeto::getTamanoX()  {return tamanox;}
+TipoEntero Objeto::getTamanoY()  {return tamanoy;}
+TipoString Objeto::getState()    {return state;}
+Boss::Boss(const TipoString& nombre, TipoCaracter color,
+               TipoEntero posX, TipoEntero posY,TipoEntero tamanox_,TipoEntero tamanoy_):Objeto(nombre, color,posX,posY){
+    tamanox=tamanox_;
+    tamanoy=tamanoy_;
+};
 
 TipoString Objeto::mostrarPosicion() {
     return "X = " + to_string(posX) + " Y = " + to_string(posY);
-}
